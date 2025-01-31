4032c6b70f64ad00fdafc8da1f8eff6f54b87e72
venicephua <venicepsy@gmail.com>
2025-01-31 18:32:38 +0800

    Implemented listen and echo

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,32 +1,56 @@
+import java.util.Scanner;
+
 public class Nova {
     public static void logo() {
-        String border = "____________________________________";
+        String border = "   _________________________________";
         String logo = """
-                 __    _ \s
-                |  \\  | |  ___  __    __  ___ _ \s
-                | |\\\\ | | / _ \\ \\ \\  / / / _ \\ |
-                | | \\\\| || |_| | \\ \\/ / | |_|  |
-                |_|  \\__| \\___/   \\__/   \\___/_|
+                    __    _ \s
+                   |  \\  | |  ___  __    __  ___ _ \s
+                   | |\\\\ | | / _ \\ \\ \\  / / / _ \\ |
+                   | | \\\\| || |_| | \\ \\/ / | |_|  |
+                   |_|  \\__| \\___/   \\__/   \\___/_|
                 """;
         System.out.println(border + "\n" + logo);
     }
 
     public static void sayHello() {
-        String border = "____________________________________";
+        String border = "   _________________________________";
         System.out.println(border);
-        System.out.println("Hey there!! I'm Nova :D");
-        System.out.println("What can I do for you?");
+        System.out.println("    Hey there!! I'm Nova :D");
+        System.out.println("    What can I do for you?");
+        System.out.println(border);
     }
 
     public static void sayBye() {
-        String border = "____________________________________";
+        String border = "   _________________________________";
         System.out.println(border);
-        System.out.println("Bye now! See you soon! ^o^");
+        System.out.println("    Bye now! See you soon! ^o^");
+        System.out.println(border);
+    }
+
+    public static void echo(String message) {
+        String border = "   _________________________________";
+        System.out.println(border);
+        System.out.println("    " + message);
+        System.out.println(border);
     }
 
     public static void main(String[] args) {
         logo();
         sayHello();
-        sayBye();
+
+
+        while (true) {
+            System.out.print("> ");
+            String line;
+            Scanner input = new Scanner(System.in);
+            line = input.nextLine();
+
+            if (line.contains("bye")) {
+                sayBye();
+                break;
+            }
+            echo(line);
+        }
     }
 }
